bb2facd095cf69e622296805188726dbef5bec47
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-04-14 22:35:45 +0200

    add oled

--- humeditySensor/humeditySensor.ino
+++ humeditySensor/humeditySensor.ino
@@ -1,5 +1,17 @@
 #include <Wire.h>
 #include <math.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 const int ADDRESS = 0x40;
 double temperature, humidity;
 
@@ -58,6 +70,12 @@ void setup() {
  Serial.begin(9600);
  sensor_init(ADDRESS);
  pinMode(A0, OUTPUT);
+ 
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+ 
 }
 
 void loop() {
@@ -69,12 +87,40 @@ void loop() {
    digitalWrite(A0, LOW);
  }
 
- Serial.print("Temperature: ");
- Serial.print((int)temperature);
- Serial.println("*C");
- Serial.print("Humidity: ");
- Serial.print((int)humidity);
- Serial.println("%");
- delay(1000);
+printText();
+
+delay(1000);
+ 
+}
+
+void printTable(){
+  
+  display.display();
+}
+
+void printText(){
+  display.clearDisplay();
+  display.setTextSize(1.5);
+  display.setTextColor(WHITE);  
+  display.setCursor(7,4);
+  display.print("Humedad");
+  display.setCursor(60,4);
+  display.print("Temperatura");
+  display.setTextSize(2);
+  display.setCursor(75,30);  
+  display.print((int)temperature);  
+  display.setCursor(100, 30);
+  display.print("C");
+  display.setCursor(10, 30);
+  display.print((int)humidity);
+  display.setCursor(38, 30);
+  display.print("%");  
+  display.drawLine(0, 0, 0, 63, WHITE);
+  display.drawLine(55, 0, 55, 63, WHITE);
+  display.drawLine(127, 0, 127, 63, WHITE);
+  display.drawLine(0, 0, 127, 0, WHITE);
+  display.drawLine(0, 15, 127, 15, WHITE);
+  display.drawLine(0, 63, 127, 63, WHITE);
+  display.display();
 }
 
